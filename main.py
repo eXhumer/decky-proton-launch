49b34c0f1665aaead1ca7fc10bf81b9f0c859c4f
eXhumer <exhumer1@protonmail.com>
2024-01-31 17:25:00 -0700

    refactor: communicate with backend via websockets
    Signed-off-by: eXhumer <exhumer1@protonmail.com>

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 from asyncio import sleep
 from subprocess import Popen
-from typing import Type
+from typing import Type, Union
 
 from decky_plugin import DECKY_PLUGIN_DIR
 
 
 class Plugin:
-    BACKEND_PROC: Popen[bytes] | None = None
+    """
+    This is a Decky plugin that starts a backend process and keeps it running.
+    """
+
+    BACKEND_PATH = f"{DECKY_PLUGIN_DIR}/bin/backend"
+    BACKEND_PROC: Union[Popen[bytes], None] = None
 
     @staticmethod
     async def _main(cls: Type["Plugin"]):
-        cls.BACKEND_PROC = Popen([f"{DECKY_PLUGIN_DIR}/bin/backend"])
+        """
+        Plugin entry hook.
+        """
+
+        cls.BACKEND_PROC = Popen([cls.BACKEND_PATH])
 
         while True:
             await sleep(1)
 
     @staticmethod
     async def _unload(cls: Type["Plugin"]):
+        """
+        Plugin unload hook.
+        """
+
         if cls.BACKEND_PROC is not None:
             cls.BACKEND_PROC.kill()
             cls.BACKEND_PROC = None
-
-    @staticmethod
-    async def reload_backend(cls: Type["Plugin"]):
-        await cls._unload()
-        cls.BACKEND_PROC = Popen([f"{DECKY_PLUGIN_DIR}/bin/backend"])
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,30 @@ import {
   definePlugin,
   staticClasses,
 } from "decky-frontend-lib";
-import { VFC } from "react";
+import { VFC, useState } from "react";
 import { FaShip } from "react-icons/fa";
 
 const Content: VFC = () => {
+  const [resp, setResp] = useState<string | null>(null);
+  const backendWS = new WebSocket("ws://localhost:3000");
+
+  backendWS.onopen = function() {
+    console.log("[open] Connection established with backend");
+    console.log("Sending \"Hello, world!\" to server");
+    backendWS.send("Hello, world!");
+  };
+
+  backendWS.onmessage = function(event) {
+    console.log(`[message] Data received from server: ${event.data}`);
+    setResp(event.data);
+  };
+
   return (
     <PanelSection title="Proton Launch">
       <h1>Hello, World!</h1>
+      {resp !== null ?
+        <h2>Backend Response: {resp}</h2> :
+        null}
     </PanelSection>
   );
 };
